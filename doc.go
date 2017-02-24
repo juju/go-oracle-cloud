55e50f12fcfd1123f10ccacf9706f653b7804b35
Giulitti Salvatore <sgiulitti@cloudbasesolutions.com>
2017-02-24 14:47:55 +0200

    Add doc.go
    Signed-off-by: Giulitti Salvatore <sgiulitti@cloudbasesolutions.com>

---
+++ doc.go
@@ -0,0 +1,8 @@
+// Copyright 2017 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+//
+// Project homepage: https://github.com/hoenirvili/go-oracle-cloud
+//
+// For more information please refer to the README file
+// in this directory.
+package go_oracle_cloud
