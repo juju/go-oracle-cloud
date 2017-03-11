a4d5b047be8322390d281da9c5e27029f1796367
Giulitti Salvatore <sgiulitti@cloudbasesolutions.com>
2017-03-11 16:05:00 +0200

    Error method testing the backup configuration resource
    Signed-off-by: Giulitti Salvatore <sgiulitti@cloudbasesolutions.com>

--- api/backupconfiguration_test.go
+++ api/backupconfiguration_test.go
@@ -1,8 +1,28 @@
 package api_test
 
 import (
+	"net/http"
+	"strings"
+
 	"github.com/juju/go-oracle-cloud/api"
 	gc "gopkg.in/check.v1"
+)
+
+var (
+	interval = map[string]map[string]int{
+		"Hourly": map[string]int{
+			"hourlyInterval": 1,
+		},
+	}
+
+	backupParams = api.BackupConfigurationParams{
+		Name:                 "/Compute-acme/jack.jones@example.com/backupConfigVol1",
+		Description:          "some random description",
+		Enabled:              false,
+		BackupRetentionCount: 2,
+		VolumeUri:            "http://api-z999.compute.us0.oraclecloud.com/storage/volume/Compute-acme/jack.jones@example.com/vol1",
+		Interval:             interval,
+	}
 )
 
 func (cl clientTest) TestBackupConfigratuionWithNoAuthentication(c *gc.C) {
@@ -12,40 +32,87 @@ func (cl clientTest) TestBackupConfigratuionWithNoAuthentication(c *gc.C) {
 
 	defer ts.Close()
 
-	interval := map[string]map[string]int{
-		"Hourly": map[string]int{
-			"hourlyInterval": 1,
-		},
-	}
-
-	params := api.BackupConfigurationParams{
-		Name:                 "/Compute-acme/jack.jones@example.com/backupConfigVol1",
-		Description:          "some random description",
-		Enabled:              false,
-		BackupRetentionCount: 2,
-		VolumeUri:            "http://api-z999.compute.us0.oraclecloud.com/storage/volume/Compute-acme/jack.jones@example.com/vol1",
-		Interval:             interval,
-	}
-
-	_, err := client.CreateBackupConfiguration(params)
+	_, err := client.CreateBackupConfiguration(backupParams)
 	c.Assert(err, gc.NotNil)
 	c.Assert(api.IsNotAuth(err), gc.Equals, true)
 
-	_, err = client.UpdateBackupConfiguration(params, params.Name)
+	_, err = client.UpdateBackupConfiguration(backupParams, backupParams.Name)
 	c.Assert(err, gc.NotNil)
 	c.Assert(api.IsNotAuth(err), gc.Equals, true)
 
-	err = client.DeleteBackupConfiguration(params.Name)
+	err = client.DeleteBackupConfiguration(backupParams.Name)
 	c.Assert(err, gc.NotNil)
 	c.Assert(api.IsNotAuth(err), gc.Equals, true)
 
-	_, err = client.BackupConfigurationDetails(params.Name)
+	_, err = client.BackupConfigurationDetails(backupParams.Name)
 	c.Assert(err, gc.NotNil)
 	c.Assert(api.IsNotAuth(err), gc.Equals, true)
 
 	_, err = client.AllBackupConfigurations(nil)
 	c.Assert(err, gc.NotNil)
 	c.Assert(api.IsNotAuth(err), gc.Equals, true)
+}
+
+func (cl clientTest) TestBackupConfigratuionWithErrors(c *gc.C) {
+	for key, val := range httpStatusErrors {
+		ts, client := cl.StartTestServerAuth(httpParams{
+			manualHeaderStatus: true,
+			check:              c,
+			body:               createResponse(c, errAPI),
+			handler: func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(key)
+			},
+		})
+
+		_, err := client.CreateBackupConfiguration(backupParams)
+		c.Assert(err, gc.NotNil)
+		c.Assert(val(err), gc.Equals, true)
+		c.Assert(
+			strings.Contains(err.Error(), errAPI.Message),
+			gc.Equals,
+			true)
+
+		// when we encounter this case that,[
+		// the delete method is recivng http.StatusNotFound
+		// this means for the delete resource point of view to not be
+		// an acutal error and it will return nil so we don't need to check this
+		if key != http.StatusNotFound {
+			err = client.DeleteBackupConfiguration(backupParams.Name)
+			c.Assert(err, gc.NotNil)
+			c.Assert(val(err), gc.Equals, true)
+			c.Assert(
+				strings.Contains(err.Error(), errAPI.Message),
+				gc.Equals,
+				true)
+		}
+
+		_, err = client.AllBackupConfigurations(nil)
+		c.Assert(err, gc.NotNil)
+		c.Assert(val(err), gc.Equals, true)
+		c.Assert(
+			strings.Contains(err.Error(), errAPI.Message),
+			gc.Equals,
+			true)
+
+		_, err = client.BackupConfigurationDetails(backupParams.Name)
+		c.Assert(err, gc.NotNil)
+		c.Assert(val(err), gc.Equals, true)
+		c.Assert(
+			strings.Contains(err.Error(), errAPI.Message),
+			gc.Equals,
+			true)
+
+		_, err = client.UpdateBackupConfiguration(backupParams, backupParams.Name)
+		c.Assert(err, gc.NotNil)
+		c.Assert(val(err), gc.Equals, true)
+		c.Assert(
+			strings.Contains(err.Error(), errAPI.Message),
+			gc.Equals,
+			true)
+
+		ts.Close()
+	}
+
 }
 
 // TODO(sgiulitti) continue to write unit tests
