f091e7f4c0e144f030e4513fa925a69e2a0c240c
Giulitti Salvatore <sgiulitti@cloudbasesolutions.com>
2017-03-21 14:47:34 +0200

    Add unit testing in common pkg.
    Signed-off-by: Giulitti Salvatore <sgiulitti@cloudbasesolutions.com>

---
+++ common/backup_test.go
@@ -0,0 +1,38 @@
+package common_test
+
+import (
+	"github.com/juju/go-oracle-cloud/common"
+
+	gc "gopkg.in/check.v1"
+)
+
+type commTest struct{}
+
+var _ = gc.Suite(&commTest{})
+
+func (commTest) TestNewInterval(c *gc.C) {
+	value := 20
+	interval := common.NewInterval(value)
+	c.Assert(interval.Hourly.HourlyInterval, gc.DeepEquals, value)
+
+	err := interval.Validate()
+	c.Assert(err, gc.IsNil)
+}
+
+func (commTest) TestNewDailyWeekly(c *gc.C) {
+	weeks := []common.Week{
+		common.Sunday,
+		common.Monday,
+	}
+	time := "03:15"
+	timezone := "America/Los_angeles"
+
+	daily := common.NewDailyWeekly(weeks, time, timezone)
+
+	c.Assert(daily.DaysOfWeek, gc.DeepEquals, weeks)
+	c.Assert(daily.TimeOfDay, gc.DeepEquals, time)
+	c.Assert(daily.UserTimeZone, gc.DeepEquals, timezone)
+
+	err := daily.Validate()
+	c.Assert(err, gc.IsNil)
+}
